963751e52b6fdffcee77075593d7841417c4d132
mcarov <mcarov@localhost>
2019-04-16 18:15:03 +0300

    2nd commit

--- src/main/java/ru/itpark/springbootitpark/controller/HelloController.java
+++ src/main/java/ru/itpark/springbootitpark/controller/HelloController.java
@@ -1,11 +1,13 @@
 package ru.itpark.springbootitpark.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
+    @GetMapping
     public String hello() {
         return "hello";
     }
